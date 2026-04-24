fb8620d4bc10fbeb6acbb16f368f64af9866ac8d
Ab50718090Ab <mithuray8708@gmail.com>
2026-04-25 02:13:25 +0600

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL=https://your-backend-name.onrender.com
     withCredentials: true,
 });
 
