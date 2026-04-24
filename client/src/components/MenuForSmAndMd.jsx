cb1a19cf49fc958cb0fb8280822301080c1b5c30
Ab50718090Ab <mithuray8708@gmail.com>
2026-04-25 04:06:22 +0600

    Update MenuForSmAndMd.jsx

--- client/src/components/MenuForSmAndMd.jsx
+++ client/src/components/MenuForSmAndMd.jsx
@@ -14,6 +14,7 @@ import { useNavigate } from "react-router-dom";
 import { useUser } from "../contexts/UserContext";
 import api from "../utils/api";
 import toast from "react-hot-toast";
+import { FaUserClock } from "react-icons/fa";
 
 function MenuForSmAndMd({ isOpen, setIsOpen }) {
     const { user, setUser } = useUser();
