b011b8ac57d0f59140aa059d4f0a0acc46af081a
Ab50718090Ab <mithuray8708@gmail.com>
2026-04-24 17:56:17 +0600

    Update GamePage.jsx

--- client/src/pages/GamePage.jsx
+++ client/src/pages/GamePage.jsx
@@ -1,18 +1,19 @@
 import { useParams } from "react-router-dom";
-import MineGames from "./Games/MineGames";
-import DiceGames from "./Games/DiceGames";
+import MinesGame from "./Games/MinesGame";
+import DiceGame from "./Games/DiceGame";
 
 const GamePage = () => {
   const { gameSlug } = useParams();
 
-  switch (gameSlug) {
-    case "mines":
-      return <MineGames />;
-    case "dice":
-      return <DiceGames />;
-    default:
-      return <h1 className="text-white">Game Not Found</h1>;
+  if (gameSlug === "mines") {
+    return <MinesGame />;
   }
+
+  if (gameSlug === "dice") {
+    return <DiceGame />;
+  }
+
+  return <h1 className="text-white text-center mt-10">Game Not Found</h1>;
 };
 
 export default GamePage;
