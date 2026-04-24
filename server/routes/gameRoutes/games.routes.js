cec7efa5f24812491037e9a0d2a0285a5f27d8ab
Ab50718090Ab <mithuray8708@gmail.com>
2026-04-24 20:51:26 +0600

    Add files via upload

--- server/routes/gameRoutes/games.routes.js
+++ server/routes/gameRoutes/games.routes.js
@@ -1,31 +1,16 @@
 import { Router } from "express";
 import minesRoutes from "./mines.routes.js";
+import { 
+    getAllGames
+} from "../../controllers/gamesControllers/games.controller.js";
 import diceRoutes from "./dice.routes.js";
-import { getAllGames } from "../../controllers/gamesControllers/games.controller.js";
 
-const router = Router();
+const gamesRoutes = Router();
 
-/**
- * ✅ ROOT: /api/games
- */
-router.get("/", (req, res) => {
-  res.json({
-    message: "Games API is working 🚀"
-  });
-});
+gamesRoutes.get("/get-all-games", getAllGames)
 
-/**
- * ✅ GET ALL GAMES
- * /api/games/get-all-games
- */
-router.get("/get-all-games", getAllGames);
+// Mount mines-related routes under /api/games/mines
+gamesRoutes.use("/mines", minesRoutes);
+gamesRoutes.use("/dice", diceRoutes);
 
-/**
- * 🎮 Sub routes
- * /api/games/mines
- * /api/games/dice
- */
-router.use("/mines", minesRoutes);
-router.use("/dice", diceRoutes);
-
-export default router;
+export default gamesRoutes;
